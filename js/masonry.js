5303fc579c746a22a64104b69d3dffc8f012d3de
phbui <101759075+phbui@users.noreply.github.com>
2023-03-16 12:34:32 -0400

    mobile fix

--- js/masonry.js
+++ js/masonry.js
@@ -1,15 +1,20 @@
 const items = document.querySelectorAll('.item');
 const container = document.querySelector('.container');
 
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 items.forEach((item) => {
   item.addEventListener('click', () => {
     const isExpanded = item.classList.toggle('item-expanded');
     if (isExpanded) {
-      if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+      if(!isMobile){
         container.style.height = '1200px';
       }
     } else {
       item.scrollTop = 0;
+      if(!isMobile){
+        container.style.height = 'auto';
+      }
     }
   });
 });
