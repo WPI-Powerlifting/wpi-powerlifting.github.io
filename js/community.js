907f057f1f4a3923f41aae19238980ef7707890c
phbui <101759075+phbui@users.noreply.github.com>
2023-03-16 15:17:07 -0400

    added gio's contact

---
+++ js/community.js
@@ -0,0 +1,7 @@
+const home = document.querySelector('#home');
+const community = document.querySelector('#community');
+
+function pageSwap() {
+    home.classList.toggle("hidden");
+    community.classList.toggle("hidden");
+}
--- js/masonry.js
+++ js/masonry.js
@@ -1,20 +1,35 @@
-const items = document.querySelectorAll('.item');
-const container = document.querySelector('.container');
+const homeItems = document.querySelectorAll('#home .item');
+const communityItems = document.querySelectorAll('#community .item');
 
-const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+function handleClick(item, container) {
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  const isExpanded = item.classList.toggle('item-expanded');
+  if (isExpanded) {
+    if(!isMobile){
+      container.style.height = '1200px';
+    }
+  } else {
+    item.scrollTop = 0;
+    if(!isMobile){
+      container.style.height = 'auto';
+    }
+  }
+}
 
-items.forEach((item) => {
-  item.addEventListener('click', () => {
-    const isExpanded = item.classList.toggle('item-expanded');
-    if (isExpanded) {
-      if(!isMobile){
-        container.style.height = '1200px';
-      }
-    } else {
-      item.scrollTop = 0;
-      if(!isMobile){
-        container.style.height = 'auto';
-      }
+homeItems.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'A'){
+      const container = document.getElementById('home');
+      handleClick(item, container);
+    }
+  });
+});
+
+communityItems.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'A'){
+      const container = document.getElementById('community');
+      handleClick(item, container);
     }
   });
 });
