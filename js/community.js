cf922e9ec595c62bd2bbf8e19f6f81ced283b623
phbui <101759075+phbui@users.noreply.github.com>
2023-03-16 15:24:46 -0400

    button text toggle

--- js/community.js
+++ js/community.js
@@ -1,7 +1,11 @@
 const home = document.querySelector('#home');
 const community = document.querySelector('#community');
+const button = document.getElementById('page-swap');
 
 function pageSwap() {
+    
+    button.innerHTML = button.innerHTML === 'OUR COMMUNITY' ? 'HOME' : 'OUR COMMUNITY';
+
     home.classList.toggle("hidden");
     community.classList.toggle("hidden");
 }
