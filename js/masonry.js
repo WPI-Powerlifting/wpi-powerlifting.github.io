cccc162e8765df7d83fbf8cffbcdd5907f527358
phbui <101759075+phbui@users.noreply.github.com>
2023-03-15 21:49:21 -0400

    fixed sizing issues

--- js/masonry.js
+++ js/masonry.js
@@ -1,10 +1,13 @@
 document.querySelectorAll('.item').forEach((item) => {
     item.addEventListener('click', () => {
+      const container = document.querySelector('.container');
       if (item.classList.contains('item-expanded')) {
         item.classList.remove('item-expanded');
         item.scrollTop = 0;
+        container.style.height = 'auto';
       } else {
         item.classList.add('item-expanded');
+        container.style.height = (item.scrollHeight + 40) + 'px';
       }
     });
   });
