e4d8ef744a484cadca22efd841ea6106b276ebdc
phbui <101759075+phbui@users.noreply.github.com>
2023-03-16 22:09:26 -0400

    added speeep

--- js/community.js
+++ js/community.js
@@ -3,9 +3,7 @@ const community = document.querySelector('#community');
 const button = document.getElementById('page-swap');
 
 function pageSwap() {
-    
     button.innerHTML = button.innerHTML === 'OUR COMMUNITY' ? 'HOME' : 'OUR COMMUNITY';
-
     home.classList.toggle("hidden");
     community.classList.toggle("hidden");
 }
